a8581015e143c36003d6126642f919caf9d01e53
Sa-ard <tumoioio@gmail.com>
2019-08-20 05:14:55 +0700

    initial git

---
+++ index.js
@@ -0,0 +1,12 @@
+//'use strict';
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const PORT = process.env.PORT || 9090
+console.log(PORT);
+
+console.log(process.env.PROJECT_NAME);
+
+
+
