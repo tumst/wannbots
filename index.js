3e96080c0dd1406d08df70e29502fb682b2d6a1e
Sa-ard <tumoioio@gmail.com>
2019-08-20 07:42:55 +0700

    change .env

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-//'use strict';
+'use strict';
 
 const express = require('express');
-const line = require('@line/bot-sdk');
+//const line = require('@line/bot-sdk');
 
-const PORT = process.env.PORT || 9090
-console.log(PORT);
+// ENV
+const PORT = process.env.PORT || 9090;
+//console.log(PORT);
 
-console.log(process.env.PROJECT_NAME);
+//console.log(process.env.PROJECT_NAME);
+
+const app = express();
+
+app.get("/test", (req, res) => {
+	var responseObj = {
+		status: true,
+		data: {text: "hello"}
+	};
+	
+	res.json(responseObj);
+});
+
+
+
+app.listen(PORT, () => {
+	console.log('application is listen on: ', PORT);
+});
 
 
 
